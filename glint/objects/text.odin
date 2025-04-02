46b1bdc05baf16ffc79bbc0bb2e8f42b089a513c
Fabrice <schaubfabrice@gmail.com>
2025-04-02 14:59:16 +0200

    shader for text

---
+++ glint/objects/text.odin
@@ -0,0 +1,9 @@
+package objects
+
+import "glint:shaders"
+import sg "sokol:gfx"
+import "vendor:fontstash"
+
+Font :: struct {}
+
+Text :: struct {}
---
+++ shaders/text.glsl
@@ -0,0 +1,35 @@
+@ctype mat4 [16]f32
+@ctype vec2 [2]f32
+@ctype vec4 [4]f32
+
+@vs vs_text
+in vec2 position;
+in vec2 texcoord;
+in vec4 color;
+
+out vec2 uv;
+out vec4 text_color;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+    uv = texcoord;
+    text_color = color;
+}
+@end
+
+@fs fs_text
+layout(binding = 0) uniform texture2D tex;
+layout(binding = 1) uniform sampler tex_sampler;
+
+in vec2 uv;
+in vec4 text_color;
+
+out vec4 frag_color;
+
+void main() {
+    float alpha = texture(sampler2D(tex, tex_sampler), uv).r;
+    frag_color = vec4(text_color.rgb, text_color.a * alpha);
+}
+@end
+
+@program text vs_text fs_text
